da041c8f6559878805696402235602f60843065e
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-26 19:07:47 +0100

    created basic default home screen

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,20 +1,31 @@
-import { useDispatch } from "react-redux"
-import { startQuiz } from "../redux/quizSlice"
-
+import { useDispatch } from "react-redux";
+import { startQuiz } from "../redux/quizSlice";
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-    const handleStartGame = () => {
-        dispatch(startQuiz())
-    }
+  const handleStartGame = () => {
+    dispatch(startQuiz());
+  };
 
   return (
-    <div>
-        <h1>Welcome to Quiz Down</h1>
+    <div className="p-2">
+        <nav className="p-4 flex items-center justify-center">
+            <h2 className="font-mono tracking-wide text-xl font-bold">QUIZDOWN</h2>
+        </nav>
+      <h1 className="text-4xl max-w-xs mx-auto text-center mt-12 md:text-6xl lg:text-7xl md:max-w-lg  lg:max-w-4xl font-serif p-2">
+        Welcome to Quiz Down
+      </h1>
 
-        <button className="border p-1" onClick={handleStartGame}>Start Game!</button>
+      <div>
+        <h2 className="text-lg font-medium max-w-md md:max-w-lg text-textColor lg:max-w-3xl lg:text-xl text-center mx-auto p-3 lg:mt-8">Answer correctly in the quickest time. The faster you answer correctly, the higher your points!</h2>
+      </div>
+
+      <div className="mx-auto border w-fit mt-4">
+        <button className="border p-1 " onClick={handleStartGame}>
+          Start Game!
+        </button>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/data.ts
@@ -0,0 +1,53 @@
+export const questions = [
+    {
+        question: 'What is the capital of Egypt?',
+        options: ['Lagos', 'London', 'Cairo', 'Abu Dhabi'],
+        answer: 3
+    },
+    {
+        question: 'What is the capital of Nigeria?',
+        options: ['Lome', 'Lagos', 'Abuja', 'Benin'],
+        answer: 2
+    },
+    {
+        question: 'What is the capital of Italy?',
+        options: ['Rome', 'Naples', 'Paris', 'Chicago'],
+        answer: 1
+    },
+    {
+        question: 'What is the capital of the United states?',
+        options: ['New York', 'Dallas', 'Washington DC', 'Florida'],
+        answer: 3
+    },
+    {
+        question: 'What is the capital of France?',
+        options: ['Zurich', 'London', 'Lyon', 'Paris'],
+        answer: 4
+    },
+    {
+        question: 'What is the capital of Iceland?',
+        options: ['Reykjavic', 'Glasgow', 'Armsterdam', 'Heisinki'],
+        answer: 1
+    },
+    {
+        question: 'What is the capital of Switzerland?',
+        options: ['Amsterdam', 'Bern', 'Zurich', 'Glasgow'],
+        answer: 2
+    },
+    {
+        question: 'What is the capital of Scotland?',
+        options: ['Glasgow', 'Turin', 'Naples', 'Aberdeen'],
+        answer: 1
+    },
+    {
+        question: 'What is the capital of Spain?',
+        options: ['Barcelona', 'Sevilla', 'Getafe', 'Madrid'],
+        answer: 4
+    },
+    {
+        question: 'What is the capital of Russia?',
+        options: ['Saints Petersburg', 'Moscow', 'Sochi', 'Altay'],
+        answer: 2
+    },
+
+]
